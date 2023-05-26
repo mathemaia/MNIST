d907b1987a26839ee91a197ae84aef9cd438410b
mathemaia <matheusmaiadasilva@gmail.com>
2023-05-26 20:18:36 -0300

    update desenho

---
+++ teste.py
@@ -0,0 +1,91 @@
+from tkinter import *
+from PIL import ImageGrab
+import os
+from tkinter.colorchooser import askcolor
+
+class Paint(object):
+    DEFAULT_PEN_SIZE = 20.0
+    DEFAULT_COLOR = 'black'
+
+    def __init__(self):
+        self.root = Tk()
+
+        self.pen_button = Button(self.root, text='pen', command=self.use_pen)
+        self.pen_button.grid(row=0, column=0)
+
+        self.brush_button = Button(self.root, text='brush', command=self.use_brush)
+        self.brush_button.grid(row=0, column=1)
+
+        self.color_button = Button(self.root, text='color', command=self.choose_color)
+        self.color_button.grid(row=0, column=2)
+
+        self.eraser_button = Button(self.root, text='eraser', command=self.use_eraser)
+        self.eraser_button.grid(row=0, column=3)
+
+        self.choose_size_button = Scale(self.root, from_=20, to=30, orient=HORIZONTAL)
+        self.choose_size_button.grid(row=0, column=4)
+
+        self.c = Canvas(self.root, bg='black', width=300, height=300)
+        self.c.grid(row=1, columnspan=5)
+
+        self.setup()
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)  # Chamar a função 'on_closing' quando a janela for fechada
+        self.root.mainloop()
+
+    def setup(self):
+        self.old_x = None
+        self.old_y = None
+        self.line_width = self.choose_size_button.get()
+        self.color = 'white'
+        self.eraser_on = False
+        self.active_button = self.pen_button
+        self.c.bind('<B1-Motion>', self.paint)
+        self.c.bind('<ButtonRelease-1>', self.reset)
+
+    def use_pen(self):
+        self.activate_button(self.pen_button)
+
+    def use_brush(self):
+        self.activate_button(self.brush_button)
+
+    def choose_color(self):
+        self.eraser_on = False
+        self.color = askcolor(color=self.color)[1]
+
+    def use_eraser(self):
+        self.activate_button(self.eraser_button, eraser_mode=True)
+
+    def activate_button(self, some_button, eraser_mode=False):
+        self.active_button.config(relief=RAISED)
+        some_button.config(relief=SUNKEN)
+        self.active_button = some_button
+        self.eraser_on = eraser_mode
+
+    def paint(self, event):
+        self.line_width = self.choose_size_button.get()
+        paint_color = 'white' if self.eraser_on else self.color
+        if self.old_x and self.old_y:
+            self.c.create_line(self.old_x, self.old_y, event.x, event.y,
+                               width=self.line_width, fill=paint_color,
+                               capstyle=ROUND, smooth=TRUE, splinesteps=36)
+        self.old_x = event.x
+        self.old_y = event.y
+
+    def reset(self, event):
+        self.old_x, self.old_y = None, None
+
+    def on_closing(self):
+        file_path = os.path.join(os.getcwd(), 'desenho.png')
+        self.save_image(file_path)
+        self.root.destroy()
+
+    def save_image(self, file_path):
+        x = self.root.winfo_rootx() + self.c.winfo_x()
+        y = self.root.winfo_rooty() + self.c.winfo_y()
+        x1 = x + self.c.winfo_width()
+        y1 = y + self.c.winfo_height()
+        ImageGrab.grab().crop((x, y, x1, y1)).save(file_path)
+
+
+if __name__ == '__main__':
+    Paint()
